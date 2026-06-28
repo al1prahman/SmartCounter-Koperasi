da6a7f1c3435febb4edd99a9a382328c03ea1849
al1prahman <rahmanalifm21@gmail.com>
2026-06-28 13:07:07 +0700

    fix: update tracking frame variable and adjust image size for improved performance

--- main.py
+++ main.py
@@ -471,7 +471,7 @@ else:
             cv2.polylines(frame, [CASHIER_ZONE], True, (53, 107, 255), 2)
             cv2.putText(frame, "KASIR", (CASHIER_ZONE[0][0], CASHIER_ZONE[0][1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (53, 107, 255), 1)
 
-            results = model.track(frame, persist=True, classes=[0], verbose=False, conf=0.25, tracker="bytetrack.yaml", imgsz=640)
+            results = model.track(frame_kecil, persist=True, tracker="bytetrack.yaml", device=0, imgsz=480)
             
             if results[0].boxes is not None and results[0].boxes.id is not None:
                 boxes = results[0].boxes.xyxy.cpu().numpy()
